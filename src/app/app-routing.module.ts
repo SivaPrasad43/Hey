0319aca459c016fb2fd629ddee1681c3b81acb3d
SivaPrasad43 <sivaprasadshinrs43@gmail.com>
2024-03-23 14:02:19 +0530

    chat page ui created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { ChatPageComponent } from './chat-page/chat-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'chat', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'chat', component: ChatPageComponent },
