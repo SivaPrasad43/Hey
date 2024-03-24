42c1a2d688bc9915a20196bf3dd0411aed8e35b2
SivaPrasad43 <sivaprasadshinrs43@gmail.com>
2024-03-24 23:00:58 +0530

    firebase configurations

---
+++ src/environment/environment.ts
@@ -0,0 +1,20 @@
+export const environment = {
+    production: false,
+    firebase: {
+
+        apiKey: "AIzaSyA4daZlf_gMz60b_3JAmvbfTXwgQOT0rA0",
+      
+        authDomain: "hey-chat-7ea1b.firebaseapp.com",
+      
+        projectId: "hey-chat-7ea1b",
+      
+        storageBucket: "hey-chat-7ea1b.appspot.com",
+      
+        messagingSenderId: "215154946435",
+      
+        appId: "1:215154946435:web:846f8a0d3097347e9646b6",
+      
+        measurementId: "G-VQ7HY4NW68"
+      
+      }
+  };
